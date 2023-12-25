03beb9d5ed20dc627c25290d5b55b7e3c09611f1
Juan Diego Montenegro Segura <134904103+Juanshiu@users.noreply.github.com>
2023-12-25 09:46:14 -0500

    v6

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://Juanshiu.github.io',
-  base: '/Astro-curso',
+  base: '/Astro-Curso',
 });
